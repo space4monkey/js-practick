cf274f65e55ee3c25e196f7547db8dd8332f8085
Igor Lapov <Freedom7day@yandex.ru>
2021-05-12 21:57:40 +0300

    start

---
+++ lesson_1/script.js
@@ -0,0 +1,21 @@
+ console.log(22+7);
+ let a=5;
+ a=8+a;
+ console.log(a);
+ //alert(a);
+ //можно использовать но не все браузеры поддерживают (для себя)
+ //out.innerHTML = '<i>Hi</i>';//позволяет использовать теги
+ //out.innerText = '<i>Hi</i>';//не брабатывает теги
+
+ //document.getElementById('out');// в текущем документе document. getElementById получить элемент по Id
+ //document.getElementById('out').innerHTML = 777;//корректный вариант
+ let p;
+ p=document.getElementById('out');
+ p=document.querySelector('#out');//работает быстрее преведущей
+ p.innerHTML = 444;//выводим данные в параграф с id="out"
+ let c= 'hello';
+ let d = 'word';
+ console.log(c+d);
+ //1. let year = присвоить год рожденья. Задача вывести колличество лет в консоль. отнять текущий год от переменной.
+ //2. вывести полученный результат в параграф.
+ //3. вывести год и зделать его курсивом
